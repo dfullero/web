aa01dd9064b104db744afc43208d7064f0302a3c
dfullero <danielafullero00@gmail.com>
2024-12-03 22:18:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("First row is made using Ibis Paint X, second row is made using Canva.")
+function myFunction() {
+   var element = document.getElementById("headere");
+   element.classList.toggle("dark-mode");
+}
